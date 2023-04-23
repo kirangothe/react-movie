1c836eb0307ceb3003081d3de725806e483d0aac
Kiran Gothe <keyrun.gothe@gmail.com>
2023-04-23 15:22:14 +0545

    first commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+import { Routes, Route, BrowserRouter } from "react-router-dom" ;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> 
+      <Routes>
+        <Route path="/*" element ={ <App />} /> 
+      </Routes>  
+    </BrowserRouter>
   </React.StrictMode>
 );
 
