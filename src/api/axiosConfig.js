82a0d07350bf8380292403a973c6a11d1d6e0186
Kiran Gothe <keyrun.gothe@gmail.com>
2023-05-15 16:02:48 +0545

    k8s

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios' ;
 
 export default axios.create({
-    baseURL : 'http://localhost:8095',
+    baseURL : process.env.REACT_APP_API_URL || 'http://localhost:8095',
     headers : {}
 }) ;
